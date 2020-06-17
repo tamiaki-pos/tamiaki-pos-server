8007b7d0ede0af41cd7967f2a4bd24a6b6735f49
Panagiotis Karatakis <karatakis@csd.auth.gr>
2020-06-17 23:31:51 +0300

    Add basic products model fields #1 * name * description * notes * price * stock * available

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -6,9 +6,53 @@ import { Application } from '../declarations';
 export default function (app: Application) {
   const sequelizeClient: Sequelize = app.get('sequelizeClient');
   const products = sequelizeClient.define('products', {
-    text: {
+    /**
+     * used for product name
+     */
+    name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    /**
+     * used for product description
+     */
+    description: {
+      type: DataTypes.STRING
+    },
+    /**
+     * used for private notes
+     */
+    notes: {
+      type: DataTypes.STRING,
+    },
+    /**
+     * used for product base price
+     */
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    /**
+     * used to check if item is available
+     * when product is ordered we update the value
+     * -1 represent unlimited amount of products
+     * =0 means that it is not available
+     * >0 means that product is in stock
+     */
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: -1
+    },
+    /**
+     * used to override the availability of
+     * the product, if value is false
+     * the product cannot be ordered
+     */
+    available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
     }
   }, {
     hooks: {

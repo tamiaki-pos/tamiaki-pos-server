955d8171b158646dbb67447ae1817df5f13d666b
Panagiotis Karatakis <karatakis@csd.auth.gr>
2020-06-17 22:46:30 +0300

    Add products model

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
+import products from './products/products.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application) {
   app.configure(users);
+  app.configure(products);
 }

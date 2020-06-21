7f59e0e567969189dfacd924cd1ee4c14034a2d4
Panagiotis Karatakis <panagiotis_11@hotmail.com>
2020-06-21 11:13:09 +0300

    Scaffold billing-session model

---
+++ src/models/billing-sessions.model.ts
@@ -0,0 +1,46 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+import { Sequelize, DataTypes } from 'sequelize';
+import { Application } from '../declarations';
+
+export default function (app: Application) {
+  const sequelizeClient: Sequelize = app.get('sequelizeClient');
+  const billingSessions = sequelizeClient.define('billing_sessions', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    tableId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    billAmount: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      defaultValue: 0
+    },
+    paidAmount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    hooks: {
+      beforeCount(options: any) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  (billingSessions as any).associate = function (models: any) {
+    models.billing_sessions.belongsTo(models.users)
+  };
+
+  return billingSessions;
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -5,6 +5,7 @@ import extras from './extras/extras.service';
 import extrasGroups from './extras-groups/extras-groups.service';
 import extrasGroupsExtras from './extras-groups-extras/extras-groups-extras.service';
 import productsExtrasGroups from './products-extras-groups/products-extras-groups.service';
+import billingSessions from './billing-sessions/billing-sessions.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application) {
@@ -14,4 +15,5 @@ export default function (app: Application) {
   app.configure(extrasGroups);
   app.configure(extrasGroupsExtras);
   app.configure(productsExtrasGroups);
+  app.configure(billingSessions);
 }
